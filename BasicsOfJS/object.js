bfb2fdd1c1725dd40e5616f1c35500adba1b7b79
Riya Verma <109965632+riya0704@users.noreply.github.com>
2024-12-27 05:50:57 +0530

    Adding Heap and stack logics

---
+++ BasicsOfJS/object.js
@@ -0,0 +1,37 @@
+const circle = {
+    radius :1,
+    location:{
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+circle.draw();
+
+// negative value
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2+2*2);
+
+
+// let str1 = 'hello';
+// let str2 = 'riya';
+// let str3 = str1 + ' '+str2; 
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1+2+"3");
+console.log(typeof 1 + typeof 2 + typeof '1');
+console.log("3"+1+2);
+console.log(2-"1"+"2");
