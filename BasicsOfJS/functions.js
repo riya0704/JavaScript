5d03fdb4acfe23f7f50c03dfcdd21d6cef12bb57
Riya Verma <109965632+riya0704@users.noreply.github.com>
2025-01-01 14:24:41 +0530

    Refactor functions.js to update addTwoNumbers function, add new functions, and provide examples for usage

--- BasicsOfJS/functions.js
+++ BasicsOfJS/functions.js
@@ -7,7 +7,56 @@ function sayMyName(){
 
 //sayMyName();
 
-function addTwoNumbers(number1, number2) {
- console.log(number1 + number2 )
+// function addTwoNumbers(number1, number2) {
+//  console.log(number1 + number2 )
+// }
+// const result = addTwoNumbers(7,8)
+// console.log(`Result of addition two number:  ${result}`); //returns output undefined
+
+function addTwoNumber(num1,num2){
+    let result = num1+num2;
+    return result;
+    //second method
+    //return num1+num2;
 }
-addTwoNumbers("3",8)
+const result = addTwoNumber(3,7);
+// console.log(`Result of two number addition : ${result}`);
+
+function loginUserMessage(username = "Sam"){
+    if(username === undefined)
+    {
+        console.log("enter a username");
+        return
+    }
+    return `${username} just logged in !`
+}
+
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("Riya"));
+// ... is called rest operator
+function calculateCardPrice (...num1) {
+    return num1;
+}
+// console.log(calculateCardPrice(10,12,14));
+const user = {
+    username : 'Riya',
+    price : '2000',
+}
+
+function handleObj(anyobject){
+ console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+ 
+}
+
+// handleObj(user);
+
+handleObj({
+    username : 'Riya',
+    price: '433'
+})
+
+const newArray = [ 1000, 3000, 2000];
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(newArray));
