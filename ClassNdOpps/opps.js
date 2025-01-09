3539fc9f7621419dae67afdbeea0ac7355ae09ed
Riya Verma <109965632+riya0704@users.noreply.github.com>
2025-01-10 01:12:42 +0530

    Add opps.js to define a user object with properties and a method for user details

---
+++ ClassNdOpps/opps.js
@@ -0,0 +1,14 @@
+const user = {
+    username: 'Riya',
+    loginCount: 8,
+    signedIn:true,
+    getUserDetails:function(){
+        console.log(this);
+        // console.log(this.loginCount);
+        // console.log(this.username);
+     }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails);
+
